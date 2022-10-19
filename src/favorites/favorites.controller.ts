26fecefc2f56145634d711fa3acad449a75b660b
malkavianson <63012624+Malkavianson@users.noreply.github.com>
2022-10-19 14:05:44 -0300

    fix: add favorite security

--- src/favorites/favorites.controller.ts
+++ src/favorites/favorites.controller.ts
@@ -19,6 +19,8 @@ import { DislikeGameDto } from "./dto/dislike.game.dto";
 import { FavoriteGameDto } from "./dto/favorite.game.dto";
 import { FavoritesService } from "./favorites.service";
 import { AuthGuard } from "@nestjs/passport";
+import { LoggedUser } from "src/auth/loggeduser.decorator";
+import { Users } from "src/users/entities/users.entities";
 
 @UseGuards(AuthGuard())
 @ApiBearerAuth()
@@ -35,21 +37,27 @@ export class FavoritesController {
 	})
 	async favoriteKindred(
 		@Body() dto: FavoriteGameDto,
+		@LoggedUser() user: Users,
 	): Promise<Favorite> {
 		return await this.favoritesService.favoriteGame(
 			dto,
+			user,
 		);
 	}
 
 	@Get("/profiles/:id")
 	@ApiOperation({
 		summary: "Returns all profiles's favorites by ID",
+		description:
+			"Insert **profileId** to show favorites",
 	})
 	async getProfileFavorites(
 		@Param("id") id: string,
+		@LoggedUser() user: Users,
 	): Promise<Favorite[]> {
 		return await this.favoritesService.getProfileFavorites(
 			id,
+			user,
 		);
 	}
 
@@ -60,7 +68,8 @@ export class FavoritesController {
 	@HttpCode(HttpStatus.NO_CONTENT)
 	async dislikeGame(
 		@Body() dto: DislikeGameDto,
+		@LoggedUser() user: Users,
 	): Promise<Favorite> {
-		return this.favoritesService.dislikeGame(dto);
+		return this.favoritesService.dislikeGame(dto, user);
 	}
 }
--- src/favorites/favorites.service.ts
+++ src/favorites/favorites.service.ts
@@ -1,8 +1,10 @@
 import {
 	Injectable,
 	NotFoundException,
+	UnauthorizedException,
 } from "@nestjs/common";
 import { Favorites, Prisma } from "@prisma/client";
+import { Users } from "src/users/entities/users.entities";
 import { PrismaService } from "../prisma/prisma.service";
 import { DislikeGameDto } from "./dto/dislike.game.dto";
 import { FavoriteGameDto } from "./dto/favorite.game.dto";
@@ -30,41 +32,110 @@ export class FavoritesService {
 
 	async favoriteGame(
 		dto: FavoriteGameDto,
+		user: Users,
 	): Promise<Favorites> {
-		const data: Prisma.FavoritesCreateInput = {
-			profile: {
-				connect: {
-					id: dto.profileId,
+		if (user.isAdmin) {
+			const data: Prisma.FavoritesCreateInput = {
+				profile: {
+					connect: {
+						id: dto.profileId,
+					},
 				},
-			},
-			games: {
-				connect: {
-					id: dto.games,
+				games: {
+					connect: {
+						id: dto.games,
+					},
 				},
-			},
-		};
-
-		return await this.prisma.favorites.create({ data });
+			};
+			return await this.prisma.favorites.create({
+				data,
+			});
+		} else {
+			const isOwner =
+				await this.prisma.profiles.findUnique({
+					where: { id: dto.profileId },
+					select: { userId: true },
+				});
+			if (isOwner.userId === user.id) {
+				const data: Prisma.FavoritesCreateInput = {
+					profile: {
+						connect: {
+							id: dto.profileId,
+						},
+					},
+					games: {
+						connect: {
+							id: dto.games,
+						},
+					},
+				};
+				return await this.prisma.favorites.create({
+					data,
+				});
+			} else {
+				throw new UnauthorizedException(
+					"Not allow to favorite a game in this user. Please verify profile.id",
+				);
+			}
+		}
 	}
 
 	async getProfileFavorites(
 		id: string,
+		user: Users,
 	): Promise<Favorites[]> {
-		return await this.prisma.favorites.findMany({
-			where: { profileId: id },
-			include: { games: true },
-		});
+		if (user.isAdmin) {
+		} else {
+			const isOwner =
+				await this.prisma.profiles.findUnique({
+					where: { id },
+					select: { userId: true },
+				});
+			if (isOwner.userId === user.id) {
+				return await this.prisma.favorites.findMany(
+					{
+						where: { profileId: id },
+						include: { games: true },
+					},
+				);
+			} else {
+				throw new UnauthorizedException(
+					"Not allow to get favorites of this profile. Please verify profile.id",
+				);
+			}
+		}
 	}
 
-	async dislikeGame({
-		favoriteId,
-	}: DislikeGameDto): Promise<Favorites> {
+	async dislikeGame(
+		{ favoriteId }: DislikeGameDto,
+		user: Users,
+	): Promise<Favorites> {
 		this.verifyIdAndReturnGameFav(favoriteId);
-
-		return this.prisma.favorites.delete({
-			where: {
-				id: favoriteId,
-			},
-		});
+		if (user.isAdmin) {
+			return this.prisma.favorites.delete({
+				where: { id: favoriteId },
+			});
+		} else {
+			const isOwner =
+				await this.prisma.favorites.findUnique({
+					where: { id: favoriteId },
+					select: {
+						profile: {
+							select: { userId: true },
+						},
+					},
+				});
+			if (isOwner.profile.userId === user.id) {
+				return this.prisma.favorites.delete({
+					where: {
+						id: favoriteId,
+					},
+				});
+			} else {
+				throw new UnauthorizedException(
+					"Not allow to delete favorites on this user. Please verify profile.id",
+				);
+			}
+		}
 	}
 }
