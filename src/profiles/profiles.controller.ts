ffeb141c54035cae940d50c97738ea56fe5f542f
malkavianson <63012624+Malkavianson@users.noreply.github.com>
2022-10-19 14:42:56 -0300

    fix: add profile security

--- src/profiles/profiles.controller.ts
+++ src/profiles/profiles.controller.ts
@@ -7,6 +7,8 @@ import {
 	Param,
 	Delete,
 	UseGuards,
+	HttpCode,
+	HttpStatus,
 } from "@nestjs/common";
 import {
 	ApiBearerAuth,
@@ -17,6 +19,8 @@ import { CreateProfileDto } from "./dto/create-profile.dto";
 import { UpdateProfileDto } from "./dto/update-profile.dto";
 import { ProfilesService } from "./profiles.service";
 import { AuthGuard } from "@nestjs/passport";
+import { LoggedUser } from "src/auth/loggeduser.decorator";
+import { Users } from "src/users/entities/users.entities";
 
 @UseGuards(AuthGuard())
 @ApiTags("Profiles")
@@ -31,8 +35,11 @@ export class ProfilesController {
 	@ApiOperation({
 		summary: "Fill a new profile state",
 	})
-	async create(@Body() dto: CreateProfileDto) {
-		return await this.profilesService.create(dto);
+	async create(
+		@Body() dto: CreateProfileDto,
+		@LoggedUser() user: Users,
+	) {
+		return await this.profilesService.create(dto, user);
 	}
 
 	@Get()
@@ -58,15 +65,24 @@ export class ProfilesController {
 	async update(
 		@Param("id") id: string,
 		@Body() dto: UpdateProfileDto,
+		@LoggedUser() user: Users,
 	) {
-		return await this.profilesService.update(id, dto);
+		return await this.profilesService.update(
+			id,
+			dto,
+			user,
+		);
 	}
 
 	@Delete(":id")
 	@ApiOperation({
 		summary: "Release one Profile state by ID",
 	})
-	async remove(@Param("id") id: string) {
-		return await this.profilesService.remove(id);
+	@HttpCode(HttpStatus.NO_CONTENT)
+	async remove(
+		@Param("id") id: string,
+		@LoggedUser() user: Users,
+	) {
+		return await this.profilesService.remove(id, user);
 	}
 }
