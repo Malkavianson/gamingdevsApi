322923a5a0eac65d77deb9efaac7e67ee27d387d
malkavianson <63012624+Malkavianson@users.noreply.github.com>
2022-11-12 00:57:32 -0300

    fix: next()

--- src/utils/middleware.logDatabase.ts
+++ src/utils/middleware.logDatabase.ts
@@ -1,23 +1,21 @@
 import { Injectable, NestMiddleware } from "@nestjs/common";
-// import { Request, Response, NextFunction } from "express";
-import { NextFunction } from "express";
+import { Request, Response, NextFunction } from "express";
 
 @Injectable()
 export class Logger implements NestMiddleware {
 	use(
-		// req: Request,
-		// res: Response,
+		req: Request,
+		res: Response,
 		next: NextFunction,
 	): void {
-		// const data = {
-		// 	method: req.method,
-		// 	baseURL: req.baseUrl,
-		// 	header: req.headers.authorization,
-		// 	body: req.body,
-		// 	status: res.statusCode,
-		// };
-		// console.log(data);
-		console.log("");
+		const data = {
+			// method: req.method,
+			baseURL: req.baseUrl,
+			// header: req.headers.authorization,
+			// body: req.body,
+			status: res.statusCode,
+		};
+		console.log(data);
 		next();
 	}
 }
